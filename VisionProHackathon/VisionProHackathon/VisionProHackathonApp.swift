f8c3320088861ed8ba61895e0dc5c7d66224ff69
Prathamesh <prathamesh.ahire1204@gmail.com>
2026-06-23 12:38:04 +1000

    push

---
+++ VisionProHackathon/VisionProHackathon/VisionProHackathonApp.swift
@@ -0,0 +1,28 @@
+
+import SwiftUI
+
+@main
+struct VisionProHackathonApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
